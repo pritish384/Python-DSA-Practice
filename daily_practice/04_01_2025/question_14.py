498621134508ea342a22ed463226595809fbf7cf
Pritish Purav <90410463+pritish384@users.noreply.github.com>
2025-01-04 23:45:34 +0530

    Add daily practice folder creation and initial question files

---
+++ daily_practice/04_01_2025/question_14.py
@@ -0,0 +1,1 @@
+# Path: ./daily_practice\04_01_2025\question_14.py
--- script.py
+++ script.py
@@ -8,6 +8,12 @@ repo_folder = "./"
 def create_daily_folder():
     today_date = datetime.now().strftime('%d_%m_%Y')
     daily_folder = os.path.join(repo_folder, 'daily_practice', today_date)
+    for i in range(1,16):
+        # make python file
+        file_name = f"question_{i}.py"
+        file_path = os.path.join(daily_folder, file_name)
+        with open(file_path, 'w') as f:
+            f.write("# Path: " + file_path + "\n")
 
     # Create the folder for today's practice
     os.makedirs(daily_folder, exist_ok=True)
